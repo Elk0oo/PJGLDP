fb3e68f8c1965a07d8b418f7684683c2bf6cabfa
nicolas lebeault <nicolas.lebeault@diiage.org>
2021-01-28 12:19:05 +0100

    push init

---
+++ Xamarin.Repository/BookingRepository.cs
@@ -0,0 +1,87 @@
+﻿using Newtonsoft.Json;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Entity;
+using Xamarin.Repository.Interfaces;
+
+namespace Xamarin.Repository
+{
+    public class BookingRepository : IBookingRepository
+    {
+        public async Task<List<Booking>> GetBookingsBySpaceId(int idspace)
+        {
+            try
+            {
+                string apiUrl = $"https://c6m3zvixpg.execute-api.us-east-1.amazonaws.com/prod/bookings/getbyspaceid/{idspace}";
+                HttpWebRequest request = (HttpWebRequest)WebRequest.Create(apiUrl);
+                request.Method = "GET";
+                request.ContentType = "application/json";
+                request.Accept = "application/json";
+
+                StreamReader responseReader = new StreamReader(request.GetResponse().GetResponseStream());
+                string responseData = await responseReader.ReadToEndAsync();
+                responseReader.Close();
+
+                return JsonConvert.DeserializeObject<List<Booking>>(responseData);
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+        public async Task<string> CreateBooking(Booking bookingObject)
+        {
+            string s = JsonConvert.SerializeObject(bookingObject);
+            try
+            {
+                var client = new RestClient("https://c6m3zvixpg.execute-api.us-east-1.amazonaws.com/prod/bookings");
+                client.Timeout = -1;
+                var request = new RestRequest(Method.POST);
+                request.AddHeader("Content-Type", "application/json");
+                request.AddParameter("application/json", JsonConvert.SerializeObject(bookingObject), ParameterType.RequestBody);
+                IRestResponse response = client.Execute(request);
+                if (response.StatusCode == HttpStatusCode.OK)
+                {
+                    ResponseLambdaObject responseLambdaObject = JsonConvert.DeserializeObject<ResponseLambdaObject>(response.Content);
+                    if (responseLambdaObject.affectedRows == 0)
+                    {
+                        return "L'horaire de la réservation n'est pas disponible!";
+                    }
+                    else
+                    {
+                        return "Réservation effectuée !";
+                    }
+                   
+                }
+                else
+                {
+                    return "Erreur lors de la création de la réservation";
+                }
+
+            }
+            catch (Exception ex)
+            {
+                return "Erreur lors de la création de la réservation";
+            }
+        }
+    }
+}
+
+
+public class ResponseLambdaObject
+{
+    public int fieldCount { get; set; }
+    public int affectedRows { get; set; }
+    public int insertId { get; set; }
+    public int serverStatus { get; set; }
+    public int warningCount { get; set; }
+    public string message { get; set; }
+    public bool protocol41 { get; set; }
+    public int changedRows { get; set; }
+}
---
+++ Xamarin.Template/Xamarin.Template/View/AddReservation.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Entity;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using Xamarin.Repository;
+using Xamarin.Template.ViewModels;
+
+namespace Xamarin.Template.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AddReservation : ContentPage
+    {
+        int idSpace = 0;
+        BookingRepository bookingRepository;
+        public AddReservation(int _idSpace)
+        {
+            InitializeComponent();
+            idSpace = _idSpace;
+            BindingContext = new ReservationViewModel();
+            bookingRepository = new BookingRepository();
+
+
+        }
+
+        private async void Valider_Clicked(object sender, System.EventArgs e)
+        {
+            Booking b = new Booking
+            {
+                Date = ((ReservationViewModel)this.BindingContext).Date.Date,
+                StartHour = ((ReservationViewModel)this.BindingContext).StartHour.Hours,
+                EndHour = ((ReservationViewModel)this.BindingContext).EndHour.Hours,
+                SpaceId = idSpace,
+                UserId = 2,
+                NbAttendees = ((ReservationViewModel)this.BindingContext).NbAttendees,
+
+            };
+
+            string message = await bookingRepository.CreateBooking(b);   
+
+            await DisplayAlert("Création de l'espace", message, "Ok");
+
+            if (!message.Contains("pas disponible") && !message.Contains("Erreur"))
+            {
+                await Navigation.PushAsync(new Reservation(idSpace));
+            }
+        }
+    }
+}
